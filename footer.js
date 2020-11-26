fb6b87d39d457168fb22cf5be478bbf45aae557c
Mauro Hernandez <maurodeveloper@hotmail.com>
2020-11-26 15:09:28 -0400

    funciona

--- footer.js
+++ footer.js
@@ -1,8 +1,19 @@
 $(document).ready(() => {
-  $("#ft__dropdown-label").click(() => {  	
-    $("#ft__dropdown-container").slideToggle(400, () => {
-    	let newScroll = $("#ft").offset().top - 160;
-    	$(window).scrollTop(newScroll);
-    });
+	let controller_dropDown = true;
+  $("#ft__dropdown-label").click(() => {
+  	if(controller_dropDown){
+    	$("#ft__dropdown-container").slideDown(400);
+    	controller_dropDown = !controller_dropDown;
+  	}else{
+    	$("#ft__dropdown-container").slideUp(400, () => {
+    		console.log('ok')
+
+    		let newScroll = $("#ft").offset().top - 210;
+	    	$(window).scrollTop(newScroll);
+    	});
+
+  
+    	controller_dropDown = !controller_dropDown;
+  	}
   });
 });
