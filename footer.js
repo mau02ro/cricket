b1dc38b131d7b03efcb694f96d0eaa6b5eae8d06
Mauro Hernandez <maurodeveloper@hotmail.com>
2020-11-26 14:59:45 -0400

    bug dropdown

--- footer.js
+++ footer.js
@@ -1,5 +1,8 @@
 $(document).ready(() => {
-  $("#ft__dropdown-label").click(() => {
-    $("#ft__dropdown-container").slideToggle(400);
+  $("#ft__dropdown-label").click(() => {  	
+    $("#ft__dropdown-container").slideToggle(400, () => {
+    	let newScroll = $("#ft").offset().top - 160;
+    	$(window).scrollTop(newScroll);
+    });
   });
 });
